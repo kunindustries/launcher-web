628a6735482f7c0c1a34bbcaf8bb93887981683c
TuckerO <54112090+TuckerO@users.noreply.github.com>
2019-11-21 12:55:19 -0600

    redirect homepage to download page immediately

---
+++ download.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Download - KunLauncher</title>
+	</head>
+	
+	<body>
+		<?php
+		
+		$version;
+		if ($fh = fopen('version/latest/version.info', 'r')) 
+		{
+			$version = fgets($fh);
+			
+			$version = substr($version, strpos($version, '=') + 2, strlen($version));
+		}		
+		
+		echo "<a href='version/latest/latest.noexe' target='_blank' download>download kunlauncher version " . $version . "</a>";
+		
+		?>
+		
+		<p>
+		instructions:<br>
+		1. download latest.noexe<br>
+		2. rename to kunlauncher.exe <a href="about.html">(why?)</a><br>
+		3. open the thing<br>
+		</p>
+		
+		<p>
+			<a href="./apps/">app list</a>
+		</p>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <html>
 	<head>
-		<title>kunlauncher</title>
+		<title>Home - KunLauncher</title>
+		<meta http-equiv="Refresh" content="0; url=http://launcher.kunindustries.com/download" />
 	</head>
-	
-	<body>
-		<?php
-		
-		$version;
-		if ($fh = fopen('version/latest/version.info', 'r')) 
-		{
-			$version = fgets($fh);
-			
-			$version = substr($version, strpos($version, '=') + 2, strlen($version));
-		}		
-		
-		echo "<a href='version/latest/latest.noexe' target='_blank' download>download kunlauncher version " . $version . "</a>";
-		
-		?>
-		
-		<p>
-		instructions:<br>
-		1. download latest.noexe<br>
-		2. rename to kunlauncher.exe<br>
-		3. open the thing<br>
-		</p>
-		
-		<p>
-			<a href="./apps/">app list</a>
-		</p>
-	</body>
 </html>
