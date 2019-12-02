9edec2a491052b11e62996d9caae4de2a060d1be
kennyrkun <kennyrmccormick+github@gmail.com>
2019-12-02 13:40:21 -0600

    Update download.php

--- download.php
+++ download.php
@@ -5,24 +5,24 @@
 	
 	<body>		
 		<p>
-		instructions:<br>
+			instructions:<br>
 
-		<?php
-		
-		$version;
-		if ($fh = fopen('version/latest/version.info', 'r')) 
-		{
-			$version = fgets($fh);
+			<?php
 			
-			$version = substr($version, strpos($version, '=') + 2, strlen($version));
-		}		
-		
-		echo "1. <a href='version/latest/latest.noexe' target='_blank' download>download kunlauncher version " . $version . "</a><br>";
-		
-		?>
-		
-		2. rename to kunlauncher.exe <a href="about.html">(why?)</a><br>
-		3. open the thing<br>
+			$version;
+			if ($fh = fopen('version/latest/version.info', 'r')) 
+			{
+				$version = fgets($fh);
+				
+				$version = substr($version, strpos($version, '=') + 2, strlen($version));
+			}		
+			
+			echo "1. <a href='version/latest/latest.noexe' target='_blank' download>download kunlauncher version " . $version . "</a><br>";
+			
+			?>
+			
+			2. rename to kunlauncher.exe <a href="about.html">(why?)</a><br>
+			3. open the thing<br>
 		</p>
 		
 		<p>
