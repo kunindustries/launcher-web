abc260f93b33f0f0e537388072fb4ad020b70ef2
kennyrkun <kennyrmccormick+github@gmail.com>
2019-12-02 13:38:35 -0600

    remove setlocalgitconfig
    this was a helper file that wasn't supposed to be committed

--- setlocalgitconifg.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /P username="github username:"
-set /p email="github email:"
-
-git config --local user.name %username%
-git config --local user.email %email%
-
-echo Set local git config.
